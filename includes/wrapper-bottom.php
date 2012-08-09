b4357742e62079c20b08096b53c555191fb95198
unknown <owner@owner-HP.(none)>
2012-08-08 22:37:12 -0400

    Added includes wrapper-bottom.php and wrapper-top.php

---
+++ includes/wrapper-bottom.php
@@ -0,0 +1,8 @@
+    <footer>
+    	<p class="rights">© 2012 punchdesign.com. All rights reserved</p>
+    </footer>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>
+  	<script src="js/jquery.centredscroller.min.js"></script>
+  	<script>$('.slideshow').centredScroller({autoAdvance:3000});</script>
+</body>
+</html>
---
+++ includes/wrapper-top.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Punch Design</title>
+   	<link href="css/general.css" rel="stylesheet">
+</head>
+
+<body>
+	<header>
+    <div class="logoholder">
+    	<img src="images/logo.png" alt="punch logo">
+    </div>
+	<nav class="menu">
+        <ul>
+			<li<?php if($page == 'home') {?> class="current"<?php } ?>><a class="hometab" href="index.php">Home</a></li>
+			<li<?php if($page == 'work') {?> class="current"<?php } ?>><a class = "worktab" href="work.php">Work</a></li>
+			<li<?php if($page == 'about') {?> class="current"<?php } ?>><a class = "aboutab" href="about.php">About</a></li>
+			<li<?php if($page == 'contact') {?> class="current"<?php } ?>><a class = "contactab" href="contact.php">Contact</a></li>	
+			
+        </ul>
+    </nav>
+	<div class="gradient"></div>
+    <!--<div class="backgradient"></div>-->
+    </header>
