5f788da602f0e7cbb67af3ed924ebab37a3e8f7e
erikabalarezo <erikabalarezo@yahoo.com>
2012-08-09 18:15:44 -0400

    Added videos.

--- animation.php
+++ animation.php
@@ -3,7 +3,9 @@ $title='Work';
 $page = 'work';
 include 'includes/wrapper-top.php';
 ?>
-    <div class="content">
+<script src="Scripts/swfobject_modified.js" type="text/javascript"></script>
+
+<div class="content">
 		<span class="breadcrumb"><a href="work.php">Work></a>Animation</span>
 	    <h2 class="animation">Animation</h2>
 		
@@ -19,7 +21,7 @@ include 'includes/wrapper-top.php';
 				</ul>
 			</div>
 			
-			<div class="animationitem last">
+			<div class="middle">
 				<h3>Title: <span class="titlewebitem">Laura's Farm Skyscraper</span> </h3>
 				<p>Animated add for a berry farm.</p>
 				<h3 class="programsused">Programs Used:</h3>
@@ -30,7 +32,7 @@ include 'includes/wrapper-top.php';
 				</ul>
 			</div>
 			
-			<div class="animationitem">
+			<div class="last">
 				<h3>Title: <span class="titlewebitem">Popcorn Popping Game</span></h3>
 				<p>Game that allows the player to dissapear popcorn when clicked. It keeps a score of popcorns created and clicked.</p>
 				<h3 class="programsused">Programs Used:</h3>
@@ -42,70 +44,98 @@ include 'includes/wrapper-top.php';
 				</ul>
 			</div>
 			
-         </div>
+  </div>
 		 
-        <div class="sidebar gamesidebar">
+  <div class="sidebar gamesidebar">
         	
-            <div class="workitemwrapper gameone">
-				
-					<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="579" height="452" id="FlashID" title="BubbleGame">
-						<param name="movie" value="BubbleGame.swf">
-						<param name="quality" value="high">
-						<param name="wmode" value="opaque">
-						<param name="swfversion" value="10.2.0.0">
-						<!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
-						<param name="expressinstall" value="Scripts/expressInstall.swf">
-						<!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
-						<!--[if !IE]>-->
-						<object type="application/x-shockwave-flash" data="BubbleGame.swf" width="579" height="452">
-							<!--<![endif]-->
-							<param name="quality" value="high">
-							<param name="wmode" value="transparent">
-							<param name="swfversion" value="10.2.0.0">
-							<param name="expressinstall" value="Scripts/expressInstall.swf">
-							<!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
-							<div>
-								<h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
-								<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
-							</div>
-							<!--[if !IE]>-->
-						</object>
-						<!--<![endif]-->
-					</object>
-				
-            	
-            </div>
-			<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="728" height="90" id="FlashID2" title="LaurasFarmSkyscraper">
-					<param name="movie" value="erika_balarezo_assignment2_skyscraper.swf">
-					<param name="quality" value="high">
-					<param name="wmode" value="opaque">
-					<param name="swfversion" value="10.2.0.0">
-					<!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
-					<param name="expressinstall" value="Scripts/expressInstall.swf">
-					<!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
-					<!--[if !IE]>-->
-					<object type="application/x-shockwave-flash" data="erika_balarezo_assignment2_skyscraper.swf" width="728" height="90">
-						<!--<![endif]-->
-						<param name="quality" value="high">
-						<param name="wmode" value="transparent">
-						<param name="swfversion" value="10.2.0.0">
-						<param name="expressinstall" value="Scripts/expressInstall.swf">
-						<!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
-						<div>
-							<h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
-							<p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
-						</div>
-						<!--[if !IE]>-->
-					</object>
-					<!--<![endif]-->
-			</object>
-			<div class="workitemwrapper farmskyscraper">
-					
-					
-					
-			</div>
+           <!-- <div class="workitemwrapper gameone">-->
+			<div>
+              <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="579" height="452" id="FlashID3" title="bubblegame">
+                <param name="movie" value="video/BubbleGame.swf" />
+                <param name="quality" value="high" />
+                <param name="wmode" value="transparent" />
+                <param name="swfversion" value="10.2.0.0" />
+                <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+                <param name="expressinstall" value="Scripts/expressInstall.swf" />
+                <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+                <!--[if !IE]>-->
+                <object type="application/x-shockwave-flash" data="video/BubbleGame.swf" width="579" height="452">
+                  <!--<![endif]-->
+                  <param name="quality" value="high" />
+                  <param name="wmode" value="transparent" />
+                  <param name="swfversion" value="10.2.0.0" />
+                  <param name="expressinstall" value="Scripts/expressInstall.swf" />
+                  <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+                  <div>
+                    <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+                    <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
+                  </div>
+                  <!--[if !IE]>-->
+                </object>
+                <!--<![endif]-->
+              </object>
+		  </div>
+			<!--<div class="workitemwrapper farmskyscraper">-->
+    <div>
+	  <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="728" height="90" id="FLVPlayer">
+			   <param name="movie" value="FLVPlayer_Progressive.swf" />
+			   <param name="quality" value="high" />
+			   <param name="wmode" value="transparent" />
+			   <param name="scale" value="noscale" />
+			   <param name="salign" value="lt" />
+			   <param name="FlashVars" value="&amp;MM_ComponentVersion=1&amp;skinName=Clear_Skin_1&amp;streamName=video/erika_balarezo_assignment2_skyscraper&amp;autoPlay=false&amp;autoRewind=false" />
+			   <param name="swfversion" value="8,0,0,0" />
+			   <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+			   <param name="expressinstall" value="Scripts/expressInstall.swf" />
+			   <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+			   <!--[if !IE]>-->
+			   <object type="application/x-shockwave-flash" data="FLVPlayer_Progressive.swf" width="728" height="90">
+			     <!--<![endif]-->
+			     <param name="quality" value="high" />
+			     <param name="wmode" value="transparent" />
+			     <param name="scale" value="noscale" />
+			     <param name="salign" value="lt" />
+			     <param name="FlashVars" value="&amp;MM_ComponentVersion=1&amp;skinName=Clear_Skin_1&amp;streamName=video/erika_balarezo_assignment2_skyscraper&amp;autoPlay=false&amp;autoRewind=false" />
+			     <param name="swfversion" value="8,0,0,0" />
+			     <param name="expressinstall" value="Scripts/expressInstall.swf" />
+			     <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+			     <div>
+			       <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+			       <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" /></a></p>
+		         </div>
+			     <!--[if !IE]>-->
+        </object>
+			   <!--<![endif]-->
+      </object>
+    </div>
+      <!--popcorn game -->
+    <object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" width="800" height="600" id="FlashID" title="popcorngame">
+              <param name="movie" value="video/popcorngame2.swf" />
+              <param name="quality" value="high" />
+              <param name="wmode" value="opaque" />
+              <param name="swfversion" value="10.2.0.0" />
+              <!-- This param tag prompts users with Flash Player 6.0 r65 and higher to download the latest version of Flash Player. Delete it if you don’t want users to see the prompt. -->
+              <param name="expressinstall" value="Scripts/expressInstall.swf" />
+              <!-- Next object tag is for non-IE browsers. So hide it from IE using IECC. -->
+              <!--[if !IE]>-->
+              <object type="application/x-shockwave-flash" data="video/popcorngame2.swf" width="800" height="600">
+                <!--<![endif]-->
+                <param name="quality" value="high" />
+                <param name="wmode" value="opaque" />
+                <param name="swfversion" value="10.2.0.0" />
+                <param name="expressinstall" value="Scripts/expressInstall.swf" />
+                <!-- The browser displays the following alternative content for users with Flash Player 6.0 and older. -->
+                <div>
+                  <h4>Content on this page requires a newer version of Adobe Flash Player.</h4>
+                  <p><a href="http://www.adobe.com/go/getflashplayer"><img src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif" alt="Get Adobe Flash player" width="112" height="33" /></a></p>
+                </div>
+                <!--[if !IE]>-->
+              </object>
+              <!--<![endif]-->
+    </object>
+  </div>
 			
-         </div>
+</div>
 
         
     </div><!--end of content-->
